678cfcb72514be0b92c97fd27c47c07ffa7b82f0
Brian Johnson <johnba4@gmail.com>
2015-09-14 12:46:05 -0400

    changed directory names and added additional dependencies

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ var ejs = require('ejs');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var urlencodedBodyParser = bodyParser.urlencoded({extended: false});
 
 //-------------------------- Uses
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(urlencodedBodyParser);
 app.use(methodOverride('_method'));
-app.set('view_engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(cookieParser());
 // app.use(session({ secret: 'secret',
 //                   resave: false,
@@ -28,3 +28,12 @@ app.use(cookieParser());
 // app.use(passport.session()); 
 
 //--------------------------
+
+app.get('/', function (req, res) {
+    var auctionsTemplate = fs.readFileSync('views/templates/auctions.html.ejs')
+    res.render('index.html.ejs', {"auctionsTemplate": auctionsTemplate});
+})
+
+app.listen(3000, function () {
+    console.log('server ready to rock');
+})
